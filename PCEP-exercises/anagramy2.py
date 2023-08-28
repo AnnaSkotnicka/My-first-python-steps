455c23fbafc7d392294749cc9ee5dee050471a14
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-08-28 12:33:08 +0200

    kilka drobnych poprawek

--- PCEP-exercises/anagramy2.py
+++ PCEP-exercises/anagramy2.py
@@ -1,20 +1,13 @@
 def tylko_litery(slowo):
-    nowe_slowo = ""
-
-    for i in slowo:
-        if i.isalpha():
-            nowe_slowo = nowe_slowo + i
-    return nowe_slowo
+    slowo = slowo.lower().strip()
+    for i in slowo:  # iteruj po literkach w słowie
+        if not i.isalpha():  # Jeśli nie jest literką
+            nowe_slowo = input("Wpisz ponownie slowo: ")  # Wpisz ponownie slowo
+            return tylko_litery(nowe_slowo)  # sprawdz wpisane nowe słowo
+    return slowo
 
 
 def anagramy(slowo1, slowo2):
-
-    slowo1 = tylko_litery(slowo1)
-    slowo2 = tylko_litery(slowo2)
-
-    if len(slowo1) != len(slowo2):
-        return False
-
     if slowo1 == slowo2:
         return False
 
@@ -27,10 +20,15 @@ def anagramy(slowo1, slowo2):
     return lista_liter_slowa_1 == lista_liter_slowa_2
 
 
-slowo1 = input("Wpisz slowo1: ")
-slowo2 = input("Wpisz slowo2: ")
+def komunikat():
+    if anagramy(slowo1, slowo2):
+        print("To sa anagramy")
+    else:
+        print("To nie są anagramy")
 
-if anagramy(slowo1, slowo2):
-    print("To sa anagramy")
-else:
-    print("To nie są anagramy")
+
+slowo1 = input("Wpisz slowo1: ")
+slowo1 = tylko_litery(slowo1)
+slowo2 = input("Wpisz slowo2: ")
+slowo2 = tylko_litery(slowo2)
+komunikat()
